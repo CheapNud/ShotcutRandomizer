e72cf70a61cc67a8b78035fde78be02fa700ca68
Brecht Vandeninden <brecht.vandeninden@mecam.be>
2025-05-17 18:22:24 +0200

    Add project files.

---
+++ Mlt.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace ShotcutRandomizer
+{
+    [XmlRoot(ElementName = "profile")]
+    public class Profile
+    {
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+        [XmlAttribute(AttributeName = "width")]
+        public string Width { get; set; }
+        [XmlAttribute(AttributeName = "height")]
+        public string Height { get; set; }
+        [XmlAttribute(AttributeName = "progressive")]
+        public string Progressive { get; set; }
+        [XmlAttribute(AttributeName = "sample_aspect_num")]
+        public string Sample_aspect_num { get; set; }
+        [XmlAttribute(AttributeName = "sample_aspect_den")]
+        public string Sample_aspect_den { get; set; }
+        [XmlAttribute(AttributeName = "display_aspect_num")]
+        public string Display_aspect_num { get; set; }
+        [XmlAttribute(AttributeName = "display_aspect_den")]
+        public string Display_aspect_den { get; set; }
+        [XmlAttribute(AttributeName = "frame_rate_num")]
+        public string Frame_rate_num { get; set; }
+        [XmlAttribute(AttributeName = "frame_rate_den")]
+        public string Frame_rate_den { get; set; }
+        [XmlAttribute(AttributeName = "colorspace")]
+        public string Colorspace { get; set; }
+    }
+
+    [XmlRoot(ElementName = "property")]
+    public class Property
+    {
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "chain")]
+    public class Chain
+    {
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "out")]
+        public string Out { get; set; }
+    }
+
+    [XmlRoot(ElementName = "entry")]
+    public class Entry
+    {
+        [XmlAttribute(AttributeName = "producer")]
+        public string Producer { get; set; }
+        [XmlAttribute(AttributeName = "in")]
+        public string In { get; set; }
+        [XmlAttribute(AttributeName = "out")]
+        public string Out { get; set; }
+
+        public int Duration => Convert.ToInt32(Math.Floor((TimeSpan.Parse(Out) - TimeSpan.Parse(In)).TotalSeconds)); // Duration in seconds
+    }
+
+    [XmlRoot(ElementName = "playlist")]
+    public class Playlist
+    {
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+        [XmlElement(ElementName = "entry")]
+        public List<Entry> Entry { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "blank")]
+        public List<Blank> Blank { get; set; }
+
+        public string Name => Property.FirstOrDefault(x => x.Name == $@"shotcut:name")?.Text ?? "system track";
+    }
+
+    [XmlRoot(ElementName = "producer")]
+    public class Producer
+    {
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "in")]
+        public string In { get; set; }
+        [XmlAttribute(AttributeName = "out")]
+        public string Out { get; set; }
+    }
+
+    [XmlRoot(ElementName = "blank")]
+    public class Blank
+    {
+        [XmlAttribute(AttributeName = "length")]
+        public string Length { get; set; }
+    }
+
+    [XmlRoot(ElementName = "track")]
+    public class Track
+    {
+        [XmlAttribute(AttributeName = "producer")]
+        public string Producer { get; set; }
+        [XmlAttribute(AttributeName = "in")]
+        public string In { get; set; }
+        [XmlAttribute(AttributeName = "out")]
+        public string Out { get; set; }
+        [XmlAttribute(AttributeName = "hide")]
+        public string Hide { get; set; }
+    }
+
+    [XmlRoot(ElementName = "transition")]
+    public class Transition
+    {
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "out")]
+        public string Out { get; set; }
+    }
+
+    [XmlRoot(ElementName = "tractor")]
+    public class Tractor
+    {
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+        [XmlElement(ElementName = "track")]
+        public List<Track> Track { get; set; }
+        [XmlElement(ElementName = "transition")]
+        public List<Transition> Transition { get; set; }
+        [XmlAttribute(AttributeName = "id")]
+        public string Id { get; set; }
+        [XmlAttribute(AttributeName = "in")]
+        public string In { get; set; }
+        [XmlAttribute(AttributeName = "out")]
+        public string Out { get; set; }
+        [XmlElement(ElementName = "properties")]
+        public Properties Properties { get; set; }
+        [XmlAttribute(AttributeName = "title")]
+        public string Title { get; set; }
+    }
+
+    [XmlRoot(ElementName = "properties")]
+    public class Properties
+    {
+        [XmlElement(ElementName = "property")]
+        public List<Property> Property { get; set; }
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+    }
+
+    [XmlRoot(ElementName = "mlt")]
+    public class Mlt
+    {
+        [XmlElement(ElementName = "profile")]
+        public Profile Profile { get; set; }
+        [XmlElement(ElementName = "chain")]
+        public List<Chain> Chain { get; set; }
+        [XmlElement(ElementName = "playlist")]
+        public List<Playlist> Playlist { get; set; }
+        [XmlElement(ElementName = "producer")]
+        public Producer Producer { get; set; }
+        [XmlAttribute(AttributeName = "producer")]
+        public string _Producer { get; set; }
+        [XmlElement(ElementName = "tractor")]
+        public List<Tractor> Tractor { get; set; }
+        [XmlAttribute(AttributeName = "LC_NUMERIC")]
+        public string LC_NUMERIC { get; set; }
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+        [XmlAttribute(AttributeName = "title")]
+        public string Title { get; set; }
+    }
+}
---
+++ SimulatedAnnealingVideoSelector.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShotcutRandomizer
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="TargetDuration"></param>
+    /// <param name="DurationWeight">This gives moderate preference for shorter videos. 0.1 to 1.0: Balanced selection with slight preference for shorter videos. 1.0 to 2.5: Moderate preference for shorter videos. 3.0 to 5.0: Strong preference for shorter videos. > 5: Extreme bias toward the shortest videos only. </param>
+    /// <param name="NumberOfVideosWeight">This adds a slight bias toward maximizing the number of videos included in the final selection. A value between 0.0 and 1.0 is usually reasonable. A higher weight(e.g., 0.5 or 0.8) may work well in scenarios where you have many short videos and want to prioritize their inclusion.</param>
+    public class SimulatedAnnealingVideoSelector(int TargetDuration = 3600, double DurationWeight = 0.5, double NumberOfVideosWeight = 0.5)
+    {
+        private static readonly Random rand = new();
+        //Set the starting temperature high to allow more freedom in early iterations.
+        private const double InitialTemperature = 10000.0;
+        //Controls how quickly the algorithm cools down. A slower cooling rate allows more exploration.
+        private const double CoolingRate = 0.002;
+        // Weight applied to duration preference. Increase for shorter video preference.
+
+        public List<Entry> SelectVideos(List<Entry> videoList)
+        {
+            // Initial random selection
+            List<Entry> currentSolution = GetRandomSelection(videoList);
+            double currentScore = GetWeightedScore(currentSolution);
+
+            List<Entry> bestSolution = new(currentSolution);
+            double bestScore = currentScore;
+
+            double temperature = InitialTemperature;
+
+            while (temperature > 1)
+            {
+                // Generate a neighboring solution by making a small change
+                List<Entry> newSolution = GetNeighboringSolution(currentSolution, videoList);
+                double newScore = GetWeightedScore(newSolution);
+
+                // Decide whether to accept the new solution
+                if (AcceptanceProbability(currentScore, newScore, temperature) > rand.NextDouble())
+                {
+                    currentSolution = newSolution;
+                    currentScore = newScore;
+                }
+
+                // Update the best solution found so far
+                int actualDuration = newSolution.Sum(v => v.Duration);
+                if (actualDuration <= TargetDuration && currentScore > bestScore)
+                {
+                    bestSolution = new List<Entry>(currentSolution);
+                    bestScore = currentScore;
+                }
+
+                // Cool down
+                temperature *= 1 - CoolingRate;
+            }
+
+            return bestSolution;
+        }
+
+        private static double AcceptanceProbability(double currentScore, double newScore, double temperature)
+        {
+            // If the new solution is better, accept it
+            if (newScore > currentScore)
+            {
+                return 1.0;
+            }
+            // Otherwise, accept it with a probability that decreases as the temperature decreases
+            return Math.Exp((newScore - currentScore) / temperature);
+        }
+
+        // Weighted score using exponential scaling with capping and considering the number of videos
+        private double GetWeightedScore(List<Entry> videos)
+        {
+            // Define a cap to prevent overflow based on the number of videos
+            long maxValue = long.MaxValue / videos.Count;
+
+            // Calculate the weighted duration with capping
+            double weightedDuration = videos.Sum(v => Math.Min((long)(v.Duration * Math.Pow(v.Duration, DurationWeight)), maxValue));
+
+            // Factor in the number of videos
+            double videoCountScore = videos.Count * NumberOfVideosWeight;
+
+            // Return the combined score: bias toward both shorter durations and more videos
+            return weightedDuration + videoCountScore;
+        }
+
+        private List<Entry> GetRandomSelection(List<Entry> videoList)
+        {
+            // Shuffle the list and take a random selection
+            List<Entry> shuffledVideos = [.. videoList.OrderBy(x => rand.Next())];
+            List<Entry> selectedVideos = [];
+            int totalDuration = 0;
+
+            foreach (var video in shuffledVideos)
+            {
+                if (totalDuration + video.Duration <= TargetDuration)
+                {
+                    selectedVideos.Add(video);
+                    totalDuration += video.Duration;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return selectedVideos;
+        }
+
+        private static List<Entry> GetNeighboringSolution(List<Entry> currentSolution, List<Entry> videoList)
+        {
+            // Make a small change to the current solution (swap one video)
+            List<Entry> newSolution = new(currentSolution);
+
+            // Choose a video to replace
+            int indexToRemove = rand.Next(newSolution.Count);
+            newSolution.RemoveAt(indexToRemove);
+
+            // Choose a new video to add that isn't already in the solution
+            Entry newVideo;
+            do
+            {
+                newVideo = videoList[rand.Next(videoList.Count)];
+            } while (newSolution.Contains(newVideo));
+
+            newSolution.Add(newVideo);
+
+            return newSolution;
+        }
+    }
+}
